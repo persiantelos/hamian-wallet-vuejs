a079643fbc27903d8f5e43680d512cd55d1368c3
mr_ceh <webmaster.webdeveloper@gmail.com>
2021-12-08 03:32:05 -0800

    fix history mode

--- src/router/index.js
+++ src/router/index.js
@@ -19,14 +19,16 @@ Vue.use(VueMeta, {
 })
 
 const router = new VueRouter({
+  // mode: process.env.IS_ELECTRON ? 'hash' : 'history',
+  // mode: 'history',
+  mode: 'hash',
+  // 
   routes,
   store,
-
   // Use the HTML5 history API (i.e. normal-looking routes)
   // instead of routes with hashes (e.g. example.com/#/about).
   // This may require some server configuration in production:
   // https://router.vuejs.org/en/essentials/history-mode.html#example-server-configurations
-  mode: 'history',
   // Simulate native-like scroll behavior when navigating to a new
   // route and using back/forward buttons.
   scrollBehavior(to, from, savedPosition) {
