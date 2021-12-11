4c1b1dbdcdf5d39dde0a4205033ebc7638247fba
mr_ceh <webmaster.webdeveloper@gmail.com>
2021-12-10 23:42:40 -0800

    remove items that do not work from the menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,62 +1,62 @@
 export const menuItems = [
     
-    {
-        id: 1,
-        label: "menuitems.menu.text",
-        isTitle: true
-    },
-    {
-        id: 2,
-        label: "menuitems.NFTs.text",
-        isTitle: false,
-        icon:'bx bx-shape-circle',
-        link: "/NFTs",
-    },
-    {
-        id: 34,
-        label: "menuitems.relink.text",
-        isTitle: false,
-        icon:'mdi mdi-link',
-        link: "/ReLink",
-    },
+    // {
+    //     id: 1,
+    //     label: "menuitems.menu.text",
+    //     isTitle: true
+    // },
+    // {
+    //     id: 2,
+    //     label: "menuitems.NFTs.text",
+    //     isTitle: false,
+    //     icon:'bx bx-shape-circle',
+    //     link: "/NFTs",
+    // },
+    // {
+    //     id: 34,
+    //     label: "menuitems.relink.text",
+    //     isTitle: false,
+    //     icon:'mdi mdi-link',
+    //     link: "/ReLink",
+    // },
     
-    {
-        id: 75,
-        label: "menuitems.utility.list.pricing",
-        link: "/pages/pricing",
-        icon: "bx-money" 
-    },
-    {
-        id: 70,
-        label: "Services",
-        icon: "bx-cloud-lightning",
-        subItems: [
-            {
-                id: 74,
-                label: "dProfile",
-                link: "/apps/file-manager/dProfile",
-                parentId: 70
-            },
-        ]
-    },
-    {
-        id: 71,
-        label: "Help",
-        icon: "bx-help-circle",
-        subItems: [
-            {
-                id: 74,
-                label: "menuitems.utility.list.faqs",
-                link: "/pages/faqs",
-                parentId: 71
-            },
-            {
-                id: 74,
-                label: "Rules",
-                link: "/help/rules",
-                parentId: 70
-            },
-        ]
-    },
+    // {
+    //     id: 75,
+    //     label: "menuitems.utility.list.pricing",
+    //     link: "/pages/pricing",
+    //     icon: "bx-money" 
+    // },
+    // {
+    //     id: 70,
+    //     label: "Services",
+    //     icon: "bx-cloud-lightning",
+    //     subItems: [
+    //         {
+    //             id: 74,
+    //             label: "dProfile",
+    //             link: "/apps/file-manager/dProfile",
+    //             parentId: 70
+    //         },
+    //     ]
+    // },
+    // {
+    //     id: 71,
+    //     label: "Help",
+    //     icon: "bx-help-circle",
+    //     subItems: [
+    //         {
+    //             id: 74,
+    //             label: "menuitems.utility.list.faqs",
+    //             link: "/pages/faqs",
+    //             parentId: 71
+    //         },
+    //         {
+    //             id: 74,
+    //             label: "Rules",
+    //             link: "/help/rules",
+    //             parentId: 70
+    //         },
+    //     ]
+    // },
 ];
 
