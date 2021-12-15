e8bd942459259502f89fcbb9e4c7cd08756c5ae4
mr_ceh <webmaster.webdeveloper@gmail.com>
2021-12-15 08:08:35 -0800

    add default component

---
+++ src/components/profile/DefaultPage.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="row">
+          <div v-for="stat of statData" :key="stat.icon" class="col-md-4">
+            <Stat :icon="stat.icon" :title="stat.title" :value="stat.value" />
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+import Stat from "@/components/profile/stat.vue";
+
+/**
+ * Contacts-Profile component
+ */
+export default {
+  components: {  Stat },
+  data() {
+    return {
+      statData: [
+        {
+          icon: "bx bx-check-circle",
+          title: "Completed Projects",
+          value: "125"
+        },
+        {
+          icon: "bx bx-hourglass",
+          title: "Pending Projects",
+          value: "12"
+        },
+        {
+          icon: "bx bx-package",
+          title: "Total Revenue",
+          value: "$36,524"
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ src/components/profile/stat.vue
@@ -0,0 +1,42 @@
+<script>
+/**
+ * Stat component -- specify the widget icon, title and value.
+ */
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    value: {
+      type: String,
+      default: ""
+    },
+    icon: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="card mini-stats-wid" :class="$store.state.layout.themeDarkMode ? 'dark-mode':''">
+    <div class="card-body">
+      <div class="media">
+        <div class="media-body">
+          <p class="text-muted fw-medium mb-2" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode':''">{{title}}</p>
+          <h4 class="mb-0" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode-lighter':''">{{value}}</h4>
+        </div>
+
+        <div class="avatar-sm align-self-center mini-stat-icon rounded-circle bg-primary">
+          <span class="avatar-title">
+            <i :class="`${icon} font-size-24`"></i>
+          </span>
+        </div>
+      </div>
+    </div>
+    <!-- end card-body -->
+  </div>
+  <!-- end card -->
+</template>
