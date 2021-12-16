09a486dab4678209b7ac027cc3f44475dfa0d5ac
mr_ceh <webmaster.webdeveloper@gmail.com>
2021-12-15 21:50:14 -0800

    add areaXnft baseURL

--- src/common/config.ts
+++ src/common/config.ts
@@ -6,5 +6,7 @@ export default class Config
     static server:string="https://megaapi.hamian-wallet.com/"; 
     static chainAddress:string="https://telos.greymass.com"; 
     static soket:SocketService;
-    static payloadOrigin:string='Mega App'
+    static payloadOrigin:string='Mega App';
+    static areaXBaseURL:string='https://api1.areaxnft.com'
+
 }
