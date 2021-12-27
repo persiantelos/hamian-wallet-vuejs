7d3a6879a8766736f4be96e4c7770f1236bca091
mr_ceh <webmaster.webdeveloper@gmail.com>
2021-12-27 06:04:20 -0800

    add methods

---
+++ src/components/reports/ParticularReports.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+       bestSellerBaseOnToken {{bestSellerBaseOnToken}}
+       buySellInfo {{buySellInfo}}
+    </div>
+</template>
+<script lang="ts">
+import {Vue , Component} from 'vue-property-decorator'
+import ReportServices from "@/services/reportServices"
+@Component({components:{}})
+export default class ParticularReports extends Vue{
+    bestSellerBaseOnToken:any=[]
+    buySellInfo:any=[]
+    accountName:string=''
+    mounted(){
+        this.getBestSellerBaseOnToken();
+        this.buySellInfo();
+    }    
+    async getBestSellerBaseOnToken(){
+        this.bestSellerBaseOnToken = await ReportServices.bestSellerBaseOnToken(this.accountName)
+    }
+    async getBuySellInfo(){
+        this.buySellInfo = await ReportServices.buySellInfo()
+    }
+}
+</script>
