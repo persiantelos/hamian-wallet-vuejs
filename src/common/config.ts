6e99b167f5dc5c9c83f6d091de59476de3916301
mr_ceh <webmaster.webdeveloper@gmail.com>
2022-01-01 04:59:03 -0800

    add local host url

--- src/common/config.ts
+++ src/common/config.ts
@@ -7,7 +7,8 @@ export default class Config
     static chainAddress:string="https://telos.greymass.com"; 
     static soket:SocketService;
     static payloadOrigin:string='Mega App';
-    static areaXBaseURL:string='https://api1.areaxnft.com';
+    // static areaXBaseURL:string='https://api1.areaxnft.com';
+    static areaXBaseURL:string='http://localhost:9201';
     static areaXBaseURL2:string='https://api.areaxnft.com';
     static persianTelos:string='https://mainnet.persiantelos.com';
 
