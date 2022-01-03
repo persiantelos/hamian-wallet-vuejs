d8834ff27501a91d1a0f03ca834f284eda4b6cab
mr_ceh <webmaster.webdeveloper@gmail.com>
2022-01-03 07:25:02 -0800

    add avatar

--- src/components/reports/PurchasedItems.vue
+++ src/components/reports/PurchasedItems.vue
@@ -4,51 +4,70 @@
             <div v-if="loader" class="col-12">
                 <Spinner v-model="loader" />
             </div>
-            <div v-if="items.value.length == 0">
+            <div v-if="items.length == 0" class="col-12 text-center">
                 <h4 class="card-title" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode':''" >List is empty</h4>
             </div> 
-            <div v-if="!loader && items.value.length!=0" class="col-12">
-                <div class="card" :class="$store.state.layout.themeDarkMode ? 'dark-mode':''" >
+            <div v-if="!loader && items.length!=0" class="col-12">
+                <div  class="card" :class="$store.state.layout.themeDarkMode ? 'dark-mode':''">
                     <div class="card-body">
-                        <h4 class="card-title" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode':''" >Purchased Items</h4>
-                        <p class="card-title-desc" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode-lighter':''" >
-                        Top {{ items.value.length}} purchased items
-                        </p>
-                        
-                        <div  class="table-responsive">
-                            <table class="table mb-0">
-                                <thead>
-                                    <tr class="text-center" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode':''">
-                                        <th>#</th>
-                                        <th>Serial</th>
-                                        <th>Seller</th>
-                                        <th>Buyer</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr class="text-center" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode-lighter':''" 
-                                    v-for="(item , index) in items" :key="index">
-                                        <th scope="row">{{index+1}}</th>
-                                        <td>{{item.serial}}</td>
-                                        <td>{{item.owner}}</td>
-                                        <td>{{item.buyer}}</td>
-                                    </tr>
-                                </tbody>
+                        <div class="row">
+                            <div class=" me-2">
+                            <h5 class="card-title mb-1" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode':''">Purchased Items</h5>
+                            </div>
+                            <p class="card-title-desc" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode-lighter':''" >
+                            Top {{ items.length}} purchased items
+                            </p>
+                        </div>
+                        <div class="table-responsive ">
+                            <table class="table align-middle table-nowrap mb-0">
+                            <tbody :class="$store.state.layout.themeDarkMode ? 'text-dark-mode':''">
+                                <tr>
+                                <th scope="col" colspan="2">Seller</th>
+                                <th scope="col">Serial</th>
+                                <th scope="col">Buyer</th>
+                                </tr>
+
+                                <tr v-for="(item , index) in items" :key="index">
+                                    <td style="width: 100px">
+                                        <img v-if="!item.userInformation.Avatar"
+                                        src="@/assets/images/users/1.jpg"
+                                        alt=""
+                                        class="avatar-md h-auto d-block rounded"
+                                        />
+                                        
+                                        <img v-if="item.userInformation.Avatar"
+                                        :src="item.userInformation.Avatar"
+                                        alt=""
+                                        class="avatar-sm h-auto d-block rounded"
+                                        />
+                                        
+                                    </td>
+                                    <td>
+                                        <h5 class="font-size-13 text-truncate mb-1">
+                                        <a href="#" class="text-dark" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode':''">
+                                            {{item.owner}}
+                                        </a>
+                                        </h5>
+                                        <p class="text-muted mb-0">{{new Date(item.created).toString().split('G')[0]}}</p>
+                                    </td>
+                                <td><i class="mdi mdi-barcode align-middle font-size-18 me-1"></i><span class="mt-1"> {{item.serial}}</span></td>
+                                <td><i class="bx bx-user align-middle font-size-15 me-1"></i> <span class="mt-1">{{item.buyer}}</span></td>
+                                </tr>
+                            </tbody>
                             </table>
                         </div>
-                        <hr class="mt-4" >
-                        <b-pagination
-                            variant="dark"
-                            v-if="items.value.length > 0"
-                            class="justify-content-center"
-                            pills
-                            v-model="skip"
-                            :total-rows="count"
-                            :per-page="top"
-                            aria-controls="my-table"
-                        ></b-pagination>
                     </div>
-
+                    <hr class="mt-4" >
+                    <b-pagination
+                        variant="dark"
+                        v-if="items.length > 0"
+                        class="justify-content-center"
+                        pills
+                        v-model="skip"
+                        :total-rows="count"
+                        :per-page="top"
+                        aria-controls="my-table"
+                    ></b-pagination>
                 </div>
             </div>
         </div>
@@ -58,6 +77,8 @@
 import {Vue,Component,Watch} from 'vue-property-decorator';
 import Spinner from "@/components/spinner/Spinner.vue";
 import ReportServices from "@/services/reportServices"
+import AccountService from '@/services/accountService';
+
 
 
 @Component({components:{Spinner}})
@@ -74,10 +95,23 @@ export default class Purchaseditems extends Vue{
         this.loader = true;
         let items = await ReportServices.getPurchasedItems(this.$store.state.currentAccount.name,this.top,skip)
         if(items.value){
-            this.items=items;
-            this.count = items.count
-            this.loader = false;
+            this.count = items.count;
+            this.getDetails(items.value)
         }
+    }
+    async getDetails(items:any){
+        for(let item of items)
+        {
+            let userInfo = await AccountService.getCollectors(item.owner);
+            if(userInfo.value){
+                item.userInformation ={}
+                let userInformation = userInfo.value[0]
+                item.userInformation=userInformation
+                this.items.push(item)
+            }
+        }
+        console.log('this is items',this.items)
+        this.loader = false;
     }
     @Watch('skip')
     skiped(newVal:any){
--- src/components/reports/Solditem.vue
+++ src/components/reports/Solditem.vue
@@ -4,42 +4,63 @@
             <div v-if="loader" class="col-12">
                 <Spinner v-model="loader" />
             </div>
-            <div v-if="items.value.length == 0">
+            <div v-if="items.length == 0" class="col-12 text-center">
                 <h4 class="card-title" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode':''" >List is empty</h4>
             </div> 
-            <div v-if="!loader && items.value.length!=0" class="col-12">
-                <div v-if="items" class="card" :class="$store.state.layout.themeDarkMode ? 'dark-mode':''" >
+            <div v-if="!loader && items.length!=0" class="col-12">
+                <div  class="card" :class="$store.state.layout.themeDarkMode ? 'dark-mode':''">
                     <div class="card-body">
-                        <h4 class="card-title" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode':''" >Sold Items</h4>
-                        <p class="card-title-desc" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode-lighter':''" >
-                        Top {{ items.value.length}} Sold items
-                        </p>
-                        
-                        <div  class="table-responsive">
-                            <table class="table mb-0">
-                                <thead>
-                                    <tr class="text-center" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode':''">
-                                        <th>#</th>
-                                        <th>Serial</th>
-                                        <th>Seller</th>
-                                        <th>Buyer</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr class="text-center" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode-lighter':''" 
-                                    v-for="(item , index) in items.value" :key="index">
-                                        <th scope="row">{{index+1}}</th>
-                                        <td>{{item.serial}}</td>
-                                        <td>{{item.owner}}</td>
-                                        <td>{{item.buyer}}</td>
-                                    </tr>
-                                </tbody>
+                        <div class="row">
+                            <div class=" me-2">
+                            <h5 class="card-title mb-1" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode':''">Sold Item</h5>
+                            </div>
+                            <p class="card-title-desc" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode-lighter':''" >
+                            Top {{ items.length}} Sold items
+                            </p>
+                        </div>
+                        <div class="table-responsive ">
+                            <table class="table align-middle table-nowrap mb-0">
+                            <tbody :class="$store.state.layout.themeDarkMode ? 'text-dark-mode':''">
+                                <tr>
+                                <th scope="col" colspan="2">Seller</th>
+                                <th scope="col">Serial</th>
+                                <th scope="col">Buyer</th>
+                                </tr>
+
+                                <tr v-for="(item , index) in items" :key="index">
+                                    <td style="width: 100px">
+                                        <img v-if="!item.userInformation.Avatar"
+                                        src="@/assets/images/users/1.jpg"
+                                        alt=""
+                                        class="avatar-md h-auto d-block rounded"
+                                        />
+                                        
+                                        <img v-if="item.userInformation.Avatar"
+                                        :src="item.userInformation.Avatar"
+                                        alt=""
+                                        class="avatar-sm h-auto d-block rounded"
+                                        />
+                                        
+                                    </td>
+                                    <td>
+                                        <h5 class="font-size-13 text-truncate mb-1">
+                                        <a href="#" class="text-dark" :class="$store.state.layout.themeDarkMode ? 'text-dark-mode':''">
+                                            {{item.owner}}
+                                        </a>
+                                        </h5>
+                                        <p class="text-muted mb-0">{{new Date(item.created).toString().split('G')[0]}}</p>
+                                    </td>
+                                <td><i class="mdi mdi-barcode align-middle font-size-18 me-1"></i><span class="mt-1"> {{item.serial}}</span></td>
+                                <td><i class="bx bx-user align-middle font-size-15 me-1"></i> <span class="mt-1">{{item.buyer}}</span></td>
+                                </tr>
+                            </tbody>
                             </table>
                         </div>
-                        <hr class="mt-4" >
+                    </div>
+                    <hr class="mt-4" >
                         <b-pagination
                             variant="dark"
-                            v-if="items.value.length > 0"
+                            v-if="items.length > 0"
                             class="justify-content-center"
                             pills
                             v-model="skip"
@@ -47,10 +68,7 @@
                             :per-page="top"
                             aria-controls="my-table"
                         ></b-pagination>
-                    </div>
-
                 </div>
-                
             </div>
         </div>
     </div>
@@ -59,6 +77,7 @@
 import {Vue,Component,Watch} from 'vue-property-decorator';
 import Spinner from "@/components/spinner/Spinner.vue";
 import ReportServices from "@/services/reportServices"
+import AccountService from '@/services/accountService';
 
 
 @Component({components:{Spinner}})
@@ -75,10 +94,23 @@ export default class Solditem extends Vue{
         this.loader = true;
         let items = await ReportServices.getSoldItems(this.$store.state.currentAccount.name,this.top,skip)
         if(items.value){
-            this.items=items;
-            this.count = items.count
-            this.loader = false;
+            this.count = items.count;
+            this.getDetails(items.value)
         }
+    }
+    async getDetails(items:any){
+        for(let item of items)
+        {
+            let userInfo = await AccountService.getCollectors(item.owner);
+            if(userInfo.value){
+                item.userInformation ={}
+                let userInformation = userInfo.value[0]
+                item.userInformation=userInformation
+                this.items.push(item)
+            }
+        }
+        console.log('this is items',this.items)
+        this.loader = false;
     }
     @Watch('skip')
     skiped(newVal:any){
